410e61f46d1771c024c7fd2b9efa01cc532100da
7910528 <7910528@stud.nau.edu.ua>
2024-05-24 15:50:05 +0300

    5th commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,10 @@ module com.tictactoe.tictactoe
     requires javafx.controls;
     requires javafx.fxml;
     requires org.junit.jupiter.api;
-    requires org.junit.platform.commons;
 
     opens com.tictactoe.tictactoe to javafx.fxml;
     opens com.tictactoe.tictactoe.controller to javafx.fxml;
-    opens com.tictactoe.tictactoe.model to org.junit.platform.commons, org.junit.jupiter.api;
+    opens com.tictactoe.tictactoe.model to org.junit.jupiter;
 
     exports com.tictactoe.tictactoe;
     exports com.tictactoe.tictactoe.controller;
